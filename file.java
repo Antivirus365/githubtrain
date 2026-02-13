72368213aed15deb1e69c4664af60b463359638e
Rashid Jamal <mdj.ug25.ec@nitp.ac.in>
2026-02-13 08:46:37 -0500

    multiply added

---
+++ file.java
@@ -0,0 +1,12 @@
+public class file {
+    public static void main(String[] args) {
+        int a = 66;
+        int b=8;
+        int sum = a+b;
+        System.out.println(sum);
+        int diff = a-b;
+        System.out.println(diff);
+    }
+
+    
+}
