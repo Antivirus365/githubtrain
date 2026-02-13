71cbf19356dd3c0058220aee1d9b0b5a75714c01
Rashid Jamal <mdj.ug25.ec@nitp.ac.in>
2026-02-13 07:37:06 -0500

    i have changed

--- stack.java
+++ stack.java
@@ -1,6 +1,7 @@
 public class stack{
     public static void main(String[] args) {
-        System.out.println("hello world");
+        
         System.out.println("thank you");
+        System.out.println("code changed");
     }
 }
